6ed3d920dee1905e7109f068304f740d2c9bdea4
Daniel John <dj71286@gmail.com>
2023-06-04 22:22:20 +0100

    Fixed css file

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,4 @@
 $('document').ready(function() {
-    const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
-    $.get(url, function (response) {
-        if (response.status === 'OK') {
-            $('DIV#api_status').addClass('available');
-        } else {
-            $('DIV#api_status').removeClass('available');
-        }
-    });
-    
     let amenities = {};
     $('INPUT[type="checkbox"]').change(function() {
         if ($(this).is(':checked')) {
@@ -15,10 +6,6 @@ $('document').ready(function() {
         } else {
             delete amenities[$(this).attr('data-id')];
         }
-        if (Object.values(amenities).length === 0) {
-            $('.amenities H4').html('&nbsp;');
-        } else {
-            $('.amenities H4').text(Object.values(amenities).join(','));
-        }
+        $('.amenities H4').text(Object.values(amenities).join(','));
     });
 });
